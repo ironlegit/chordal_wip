779c9d458b7ea27909c34380a6fd5f31a2ace30a
LionelRohner <lionel.rohner@gmail.com>
2026-03-19 01:44:50 -0400

    adapt unit tests 5/11

--- tests/test_chordcanonizer.py
+++ tests/test_chordcanonizer.py
@@ -4,24 +4,46 @@ import pytest
 cc = ChordCanonizer()
 
 
-def test_decompose_1():
+def test_decompose():
     test = "Ebmaj7add9/G"
 
     actual = cc._decompose(test)
+
     expected = {
         "root": "Eb",
         "quality": "maj",
+        "quality_5th": None,
+        "quality_7th": None,
         "adds": ["add9"],
-        "sus": None,
         "dominant": None,
         "extensions": ["7"],
-        "alterations": [],
         "slash": "G",
         "unclear": [],
     }
     assert actual == expected, f"Expected {expected}, got {actual}"
 
 
+def test_normalize():
+    test = "Ebmaj7add9/G"
+
+    decomp = cc._decompose(test)
+    actual = cc._normalize(decomp)
+
+    expected = {
+        "root": "Eb",
+        "quality": "maj",
+        "quality_5th": None,
+        "quality_7th": "maj",
+        "adds": ["add9"],
+        "dominant": None,
+        "extensions": [],
+        "slash": "G",
+        "unclear": [],
+    }
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
+@pytest.mark.skip(reason="rethink edge case handling!")
 def test_edge_cases_1():
     test = "E13-"
 
@@ -53,25 +75,34 @@ def test_canonicalize_triads():
     test = "Emin Em E- EMaj Emaj EM"
 
     actual = cc.canonicalize(test)
-    expected = "E(q:m) E(q:m) E(q:m) E(q:maj) E(q:maj) E(q:maj)"
+    expected = "E(q3:m) E(q3:m) E(q3:m) E(q3:maj) E(q3:maj) E(q3:maj)"
 
     assert actual == expected, f"Expected {expected}, got {actual}"
 
 
 def test_canonicalize_aug_1():
-    test = "C5+ C+5 C7+ Caug C+"
+    test = "C5+ C+5 Caug C+"
 
     actual = cc.canonicalize(test)
-    expected = "C(a:#5) C(e:5)(a:#5) C(d:True)(e:7)(a:#5) C(a:#5) C(a:#5)"
+    expected = "C(q3:maj)(q5:aug) C(q3:maj)(q5:aug) C(q3:maj)(q5:aug) C(q3:maj)(q5:aug)"
 
     assert actual == expected, f"Expected {expected}, got {actual}"
 
 
 def test_canonicalize_aug_2():
-    test = "C7+ Caug7 C7+/9 C+M7 Cmaj7/#5"
+    test = "C7+ Caug7 C7+/9"
 
     actual = cc.canonicalize(test)
-    expected = "X X X X X"
+    expected = "C(q3:maj)(q5:aug)(q7:m) C(q3:maj)(q5:aug)(q7:m) C(q3:maj)(q5:aug)(q7:m)(e:9)"
+
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
+def test_canonicalize_aug_3():
+    test = "C+M7 Cmaj7/#5"
+
+    actual = cc.canonicalize(test)
+    expected = "C(q3:maj)(q5:aug)(q7:maj) C(q3:maj)(q5:aug)(q7:maj)"
 
     assert actual == expected, f"Expected {expected}, got {actual}"
 
@@ -80,7 +111,9 @@ def test_canonicalize_sus():
     test = "Csus Csus2 Csus4 Csus9 Csus/E"
 
     actual = cc.canonicalize(test)
-    expected = "C(s:sus4) C(s:sus2) C(s:sus4) C(s:sus4)(e:9) C(s:sus4)/E"
+    expected = (
+        "C(q3:sus4) C(q3:sus2) C(q3:sus4) C(q3:sus4)(q7:m)(e:9) C(q3:sus4)/E"
+    )
 
     assert actual == expected, f"Expected {expected}, got {actual}"
 
@@ -89,7 +122,7 @@ def test_canonicalize_dim():
     test = "Cdim D#dim7 Dbdim Cdim/Ab"
 
     actual = cc.canonicalize(test)
-    expected = "C(q:dim) D#(q:dim)(e:7) Db(q:dim) C(q:dim)/Ab"
+    expected = "C(q5:dim) D#(q5:dim)(q7:dim) Db(q5:dim) C(q5:dim)/Ab"
 
     assert actual == expected, f"Expected {expected}, got {actual}"
 
@@ -98,7 +131,7 @@ def test_canonicalize_slash():
     test = "E#/Cb E#7/9/Cb C/D Ebsus4(7)/C#"
 
     actual = cc.canonicalize(test)
-    expected = "E#(q:maj)/Cb E#(d:True)(e:7,9)/Cb C(q:maj)/D Eb(s:sus4)(e:7)/C#"
+    expected = "E#(q3:maj)/Cb E#(q3:maj)(q7:m)(e:9)/Cb C(q3:maj)/D Eb(q3:sus4)(q7:m)/C#"
 
     assert actual == expected, f"Expected {expected}, got {actual}"
 
