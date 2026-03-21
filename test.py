8baf11e85d86ff176798bb7d45677526414e39a3
LionelRohner <lionel.rohner@gmail.com>
2026-03-20 22:56:51 -0400

    test works

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from datasets import load_dataset
 import pandas as pd
 import re
-from chordal_wip.chordcleaner import ChordCleaner, ChordIsolator
+from chordal_wip.chordisolator import ChordIsolator
+from chordal_wip.chordcanonizer import ChordCanonizer
 
-cc = ChordCleaner(freq_threshold=3)
-cc = ChordIsolator()
+ci = ChordIsolator()
+cc = ChordCanonizer()
 
 ds = load_dataset("lluccardoner/melodyGPT-song-chords-text-1")
 
@@ -15,35 +16,40 @@ ds = ds[ds["chords_str"].notna()]
 # Filter by pop
 # ds_pop = ds[ds["genres"].str.contains("jazz", case=False)]
 
-series = ds["chords_str"]
+print("head")
 
-if False:
-    ds["chords_str_clean"] = cc.clean(series)
+ds["chords_str_clean"] = ds["chords_str"].apply(ci.raw_chord_isolation)
+ds["chords_str_canonized"] = ds["chords_str_clean"].apply(cc.canonicalize)
 
-
-ds["chords_str_clean"] = series.apply(cc.raw_chord_isolation)
-
-cnts = ds["chords_str_clean"].str.split().explode().value_counts()
-
-cnts = cnts[cnts.index.str.contains("[+-]+")]
-
-cnts.to_csv("test_cnts.csv")
-
-
-# TODO: Move into ChordCleaner
-unique_words = ds["chords_str_clean"].str.split().explode().drop_duplicates()
-word_length_df = unique_words.reset_index(drop=True).to_frame()
-word_length_df["length"] = word_length_df["chords_str_clean"].str.len()
-word_length_df.to_csv("test_cnt_len.csv")
-exit()
-cnts = ds["chords_str_clean"].str.split().explode().value_counts()
-print(len(ds["chords_str_clean"].str.split().explode()))
-
-# cnts.to_csv("test_cnts.csv", header=True)
-
+print(ds.head())
+exit
+# ds["chords_str_clean"] = ds["chords_str"].str.split().explode().value_counts()
+# # cnts = cnts[cnts.index.str.contains("[+-]+")]
+# cnts_before = cnts[cnts.index.str.contains("[#b+-]{1}(2|4|6|7|9|11|13){1}")]
+# cnts_before = cnts_before[cnts_before.index.str.len() > 4]
 #
-# ds_pop["chords_str_clean"] = cc.clean(series)
-# print(f"nrow: {series.shape[0]}")
+# cnts_after = cnts[cnts.index.str.contains("(2|4|6|7|9|11|13){1}[#b+-]{1}")]
+# cnts_after = cnts_after[cnts_after.index.str.len() > 4]
+#
+# cnts_before.to_csv("test_cnts_before.csv")
+# cnts_after.to_csv("test_cnts_after.csv")
 #
 #
-# ds_pop.iloc[0:1000].to_csv("test.csv")
+# exit()
+# # TODO: Move into ChordCleaner
+# unique_words = ds["chords_str_clean"].str.split().explode().drop_duplicates()
+# word_length_df = unique_words.reset_index(drop=True).to_frame()
+# word_length_df["length"] = word_length_df["chords_str_clean"].str.len()
+# word_length_df.to_csv("test_cnt_len.csv")
+# exit()
+# cnts = ds["chords_str_clean"].str.split().explode().value_counts()
+# print(len(ds["chords_str_clean"].str.split().explode()))
+#
+# # cnts.to_csv("test_cnts.csv", header=True)
+#
+# #
+# # ds_pop["chords_str_clean"] = cc.clean(series)
+# # print(f"nrow: {series.shape[0]}")
+# #
+# #
+# # ds_pop.iloc[0:1000].to_csv("test.csv")
