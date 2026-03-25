d5a88724016e8f7a71ff632c4c899064e519599c
LionelRohner <lionel.rohner@gmail.com>
2026-03-25 17:03:06 -0400

    added tests for ChordIsolator

--- tests/test_chordisolator.py
+++ tests/test_chordisolator.py
@@ -58,7 +58,47 @@ def test_not_reject():
     assert actual == expected, f"Expected {expected}, got {actual}"
 
 
-def test_raw_chord_isolation():
+def test_isolate_slash_extensions():
+    test = "A7/9/11 A7/b9/b11 A7/9b/11b A7/-9/-11 A7/9-/11- F#/-7b5"
+
+    actual = cc.isolate(test)
+    expected = "A7/9/11 A7/b9/b11 A7/9b/11b A7/-9/-11 A7/9-/11- F#/-7b5"
+
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
+def test_isolate_slash_quality():
+    test = "A/maj7"
+
+    actual = cc.isolate(test)
+    expected = "A/maj7"
+
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
+# Hypothetical cases
+def test_isolate_slash_alterations():
+    test = "A7/- A7/b"
+
+    actual = cc.isolate(test)
+    expected = "A7/- A7/b"
+
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
+def test_reject_slash_clusters():
+    test = ["G5/B5/A5/G5", "F/Bb-Bb-Eb-F/Bb-Bb"]
+
+    actual = []
+    for t in test:
+        actual.append(cc._reject(t))
+
+    expected = [True, True]
+
+    assert actual == expected, f"Expected {expected}, got {actual}"
+
+
+def test_isolate():
     test = """
     #notchords
     Chorus
@@ -103,7 +143,7 @@ def test_raw_chord_isolation():
     Eb/9
     """
 
-    actual = cc.raw_chord_isolation(test)
+    actual = cc.isolate(test)
     expected = """
     Amin
     Amaj7(13)
