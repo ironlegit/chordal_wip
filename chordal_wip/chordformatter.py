653185816da66f9a6be97858c19c41e8b9d3fdf9
LionelRohner <lionel.rohner@gmail.com>
2026-04-08 16:35:39 -0400

    drafted basic formatter for key prediciton

--- chordal_wip/chordformatter.py
+++ chordal_wip/chordformatter.py
@@ -1,21 +1,36 @@
-class ChordFormatter:
-    def __init__(self, type="default"):
-        self.type = type
-        self.tidy_chord = self.tidy_up(self.type)
+import re
 
-    def tidy_up(self, type: str) -> str:
-        pass
+
+class ChordFormatter:
+    ROOT_REGEX = re.compile(r"^[A-G](?:#|b)?")
+    TRIAD_MAP = {
+        "(q5:dim)": "dim",
+        "(q3:maj)": "maj",
+        "(q3:m)": "m",
+    }
+
+    def __init__(self, type="simple"):
+        self.type = type
+
+    def format(self, chords: str) -> str:
+        if self.type == "simple":
+            chords_out = []
+
+            for chord in chords.split():
+                root = self.ROOT_REGEX.match(chord).group()
+                for pattern, quality in self.TRIAD_MAP.items():
+                    if pattern in chord:
+                        chords_out.append(root + quality)
+
+        return " ".join(chords_out)
 
     def _use_unicode_flat(self):
         """Convert b and dim to ♭ and °"""
         pass
 
-    def _format_extensions(self, extensions):
-        extensions_formatted = ""
-        if extensions:
-            extensions_formatted += extensions[0]
-            if len(extensions) > 1:
-                extensions_formatted += "(e:" + ",".join(extensions[1:]) + ")"
-
 
 cf = ChordFormatter()
+
+chords = "C#(q3:m) F#(q3:maj) G#(q3:m) C#(q3:m) F#(q3:maj) G#(q3:m) C#(q3:m) F#(q3:maj) G#(q3:m) C#(q3:m) F#(q3:maj) G#(q3:m) C#(q3:m) F#(q3:maj) G#(q3:m) C#(q3:m) F#(q3:maj) G#(q3:m) C#(q3:m) F#(q3:maj) G#(q3:m) C#(q3:m) F#(q3:maj) G#(q3:m) C#(q3:m) F#(q3:maj) G#(q3:m) C#(q3:m) F#(q3:maj) G#(q3:m)"
+
+print(cf.format(chords))
