611b534c697956d19cf1b02a6df83b52db99c978
LionelRohner <lionel.rohner@gmail.com>
2026-04-09 10:02:07 -0400

    adapted quality notation for triads in chord tests

--- tests/test_chords.py
+++ tests/test_chords.py
@@ -11,14 +11,22 @@ def test_C_ionian_chord_generation():
     chord = Chord(scale)
 
     # Expected triads
-    expected_chords = ["Cmaj", "Dmin", "Emin", "Fmaj", "Gmaj", "Amin", "Bdim"]
+    expected_chords = ["Cmaj", "Dm", "Em", "Fmaj", "Gmaj", "Am", "Bdim"]
     actual_chords = list(chord.data["triads"])
     assert actual_chords == expected_chords, (
         f"Expected {expected_chords}, got {actual_chords}"
     )
 
     # Expected 7th chords
-    expected_chords = ["Cmaj7", "Dmin7", "Emin7", "Fmaj7", "G7", "Amin7", "Bmin7♭5"]
+    expected_chords = [
+        "Cmaj7",
+        "Dmin7",
+        "Emin7",
+        "Fmaj7",
+        "G7",
+        "Amin7",
+        "Bmin7♭5",
+    ]
     actual_chords = list(chord.data["7ths"])
     assert actual_chords == expected_chords, (
         f"Expected {expected_chords}, got {actual_chords}"
@@ -32,14 +40,22 @@ def test_D_dorian_chord_generation():
     chord = Chord(scale)
 
     # Expected triads
-    expected_chords = ["Dmin", "Emin", "Fmaj", "Gmaj", "Amin", "Bdim", "Cmaj"]
+    expected_chords = ["Dm", "Em", "Fmaj", "Gmaj", "Am", "Bdim", "Cmaj"]
     actual_chords = list(chord.data["triads"])
     assert actual_chords == expected_chords, (
         f"Expected {expected_chords}, got {actual_chords}"
     )
 
     # Expected 7th chords
-    expected_chords = ["Dmin7", "Emin7", "Fmaj7", "G7", "Amin7", "Bmin7♭5", "Cmaj7"]
+    expected_chords = [
+        "Dmin7",
+        "Emin7",
+        "Fmaj7",
+        "G7",
+        "Amin7",
+        "Bmin7♭5",
+        "Cmaj7",
+    ]
     actual_chords = list(chord.data["7ths"])
     assert actual_chords == expected_chords, (
         f"Expected {expected_chords}, got {actual_chords}"
@@ -53,14 +69,30 @@ def test_F_myxolydian_chord_generation():
     chord = Chord(scale)
 
     # Expected triads
-    expected_chords = ["F#maj", "G#min", "A#dim", "Bmaj", "C#min", "D#min", "Emaj"]
+    expected_chords = [
+        "F#maj",
+        "G#m",
+        "A#dim",
+        "Bmaj",
+        "C#m",
+        "D#m",
+        "Emaj",
+    ]
     actual_chords = list(chord.data["triads"])
     assert actual_chords == expected_chords, (
         f"Expected {expected_chords}, got {actual_chords}"
     )
 
     # Expected 7th chords
-    expected_chords = ["F#7", "G#min7", "A#min7♭5", "Bmaj7", "C#min7", "D#min7", "Emaj7"]
+    expected_chords = [
+        "F#7",
+        "G#min7",
+        "A#min7♭5",
+        "Bmaj7",
+        "C#min7",
+        "D#min7",
+        "Emaj7",
+    ]
     actual_chords = list(chord.data["7ths"])
     assert actual_chords == expected_chords, (
         f"Expected {expected_chords}, got {actual_chords}"
