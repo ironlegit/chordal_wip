8334bfa27678abfd6628a1495f0defc2f826827b
LionelRohner <lionel.rohner@gmail.com>
2026-04-09 09:59:18 -0400

    rm main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from chordal_wip.scales import MarkovChordProgression, Scale, Chord
-
-scale_ref = Scale("C", "dorian")
-print(scale_ref.scales_dict.keys().tolist)
-# print("C ionian:", scale_ref.notes)
-chord = Chord(scale_ref)
-# print(list(chord.data["triads"]))
-chord_progression = MarkovChordProgression(n_chords=4, chord=chord)
-# print(vars(chord_progression))
